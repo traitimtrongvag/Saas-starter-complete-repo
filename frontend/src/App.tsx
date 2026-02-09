acab2eaff29326e116e0dd28e077e755ea819745
traitimtrongvag <pan2512811@gmail.com>
2026-02-09 12:14:46 +0000

    refactor: improve code formatting in app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Todos from './pages/Todos'
+import React from 'react';
+import Todos from './pages/Todos';
 
-export default function App(){
+export default function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <header className="bg-white shadow p-4">SaaS Starter</header>
-      <main className="p-6"><Todos/></main>
+      <header className="bg-white shadow p-4">
+        <h1 className="text-xl font-semibold">SaaS Starter</h1>
+      </header>
+      <main className="p-6">
+        <Todos />
+      </main>
     </div>
-  )
+  );
 }
